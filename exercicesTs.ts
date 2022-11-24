46ef76ea2abc158bc382edb37b498dda8dc513c1
Corentin Dumontier <corentindumontier@gmail.com>
2022-11-24 11:50:48 +0100

    feat:Ajout de l'exercice 3
    doc:Correction des commentaires de l'exercice 2

--- exercicesTs.ts
+++ exercicesTs.ts
@@ -8,6 +8,8 @@ const bonjour = (prenom : string) : void => {
     console.log(`Bonjour ${prenom}`);
 }
 
+//Exercice 2
+
 /**
  * Fonction qui somme deux nombres et renvoie le résultat en console
  * @param nombre1 : Premier nombre à sommer
@@ -15,4 +17,21 @@ const bonjour = (prenom : string) : void => {
  */
 const somme = (nombre1 : number, nombre2 : number) : void => {
     console.log(nombre1+nombre2);
+}
+
+// Exercice 3
+
+/**
+ * Fonction qui donne le gain ou la perte d'un produit
+ * @param prixVente : Prix de Vente du produit
+ * @param prixFabrication : Prix de Fabrication du produit
+ * @returns Gain ou perte de la vente du produit
+ */
+const benefice = (prixVente : number, prixFabrication : number) : string => {
+    const prixRevient : number = prixVente - prixFabrication;
+    if (prixRevient >= 0) { 
+        return `Gain de ${prixRevient}€`;
+    }else{
+        return `Perte de ${-1*prixRevient}€`;
+    }
 }
