66456cf3d7dc101f4ab5e7d028c8805421f25b00
Corentin Dumontier <corentindumontier@gmail.com>
2022-11-24 11:18:17 +0100

    feat:Ajout de l'exercice 1

---
+++ exercicesTs.ts
@@ -0,0 +1,9 @@
+//Exercice 1
+
+/**
+ * Fonction qui dit bonjour à une personne en console
+ * @param prenom : prenom de la personne à qui dire bonjour
+ */
+const bonjour = (prenom : string) : void => {
+    console.log(`Bonjour ${prenom}`);
+}
